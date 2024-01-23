ef0478995032e14b52146006a8fcd8c01e0fba50
Isha Juneja <isha.juneja@gmail.com>
2024-01-23 09:51:45 -0500

    added cleaned data

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,44 +1,51 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the raw TTC Streetcar Delay data and categorizes each type of delay
+# Author: Isha Juneja
+# Date: 23 January 2024
+# Contact: isha.juneja@mail.utoronto.ca 
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: 01-download_data.R
 
 #### Workspace setup ####
+library(opendatatoronto)
 library(tidyverse)
+library(dplyr)
+
+
+readr::read_csv("inputs/data/ttc-streetcar-delay-data-2023.csv")
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv(file="inputs/data/ttc-streetcar-delay-data-2023.csv", show_col_types=FALSE)
+
+raw_data <-select(raw_data, Line, Time, `Min Delay`)
 
 cleaned_data <-
   raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+  rename(
+    StreetcarLine = Line,
+    Hour = Time,
+    MinDelay = `Min Delay`
+  )
+
+
+cleaned_data <-
+  cleaned_data %>%
+  mutate(TypeofDelay = case_when(MinDelay <= 5 ~ "No delay",
+                                 MinDelay <= 15 ~ "Small delay",
+                                 MinDelay <= 30 ~ "Big delay"))
+
+
+NoDelay <-
+  cleaned_data |> filter(TypeofDelay == "No delay") %>% group_by(Hour) %>% tally()
+
+SmallDelay <-
+  cleaned_data |> filter(TypeofDelay == "Small delay") %>% group_by(Hour) %>% tally()
+
+BigDelay <-
+  cleaned_data |> filter(TypeofDelay == "Big delay") %>% group_by(Hour) %>% tally()
+
+
+cleaned_data
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_csv(cleaned_data, "outputs/data/cleaned_ttc-streetcar-delay-2023_data.csv")
